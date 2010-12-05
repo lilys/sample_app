1c95a5b4dcd65043e4bc80fc3986d802361bb630
unknown <Admin@.(none)>
2010-12-04 21:21:04 -0300

    Modifico application

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	def title
+	    base_title = "Ruby on Rails Tutorial de ejemplo de la aplicación"
+	    if @title.nil?
+	      base_title
+		else
+	      "#{base_title} | #{@title}"
+		end
+	end
 end
